85ecb98fb7b3b4a7604c0c5e93fa6b1afc07b71a
KoblizekXD <janprokupek@centrum.cz>
2023-08-31 10:09:14 +0200

    feat: registry api

---
+++ src/main/java/lol/koblizek/minemmo/core/registry/Registry.java
@@ -0,0 +1,29 @@
+package lol.koblizek.minemmo.core.registry;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public abstract class Registry<T> {
+    private final Map<RegistryKey<T>, T> data = new HashMap<>();
+
+    public static <E> RegistryKey<E> register(Registry<E> registry, String key, E data) {
+        return registry.registerNew(data, key);
+    }
+    private RegistryKey<T> registerNew(T object, String stringedKey) {
+        RegistryKey<T> key = new RegistryKey<>(this, stringedKey, object);
+        data.put(key, object);
+        return key;
+    }
+    private T get(String key) {
+        AtomicReference<T> toReturn = new AtomicReference<>(null);
+        data.forEach((reg, obj) -> {
+            if (reg.getKey().equals(key))
+                toReturn.set(obj);
+        });
+        return toReturn.get();
+    }
+    private T get(RegistryKey<T> key) {
+        return data.get(key);
+    }
+}
